df9405d99eab441c14c958bc50732b13ae150c97
SunilKadaganchi <sunilkadaganchi071@gmail.com>
2024-12-23 11:03:25 +0530

    Intial push

---
+++ InfyBook/src/main/java/com/Infy/InfyBook/InfyBookApplication.java
@@ -0,0 +1,13 @@
+package com.Infy.InfyBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfyBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfyBookApplication.class, args);
+	}
+
+}
---
+++ InfyBook/src/test/java/com/Infy/InfyBook/InfyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Infy.InfyBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfyBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
